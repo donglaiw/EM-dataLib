95f0c5740c71e4f8781e0acfe41055cc4c53c03f
donglaiw <donglai@rclogin05.rc.fas.harvard.edu>
2018-07-06 11:55:28 -0400

    init

--- em_data/augmentation/misalign.py
+++ em_data/augmentation/misalign.py
@@ -10,6 +10,7 @@ Kisuk Lee <kisuklee@mit.edu>, 2016-2017
 import numpy as np
 
 from em_data.augmentor import DataAugment
+from em_segLib.seg_util import check_tensor
 
 class Misalign(DataAugment):
     """
